3e01355d01e4fa3246d8a07d4c8c09cfe997e823
boris.uvarov <uvarov.b@gmail.com>
2021-08-22 17:07:53 +0300

    Update radio analog read

--- arduino/radio/src/main.cpp
+++ arduino/radio/src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
-#include "GyverTM1637.h"
 #include <Wire.h>
 #include <radio.h>
 #include <TEA5767.h>
 
 #define MIN_POTENTIOMETER_VALUE 0
 #define MAX_POTENTIOMETER_VALUE 1023
+#define POTENTIOMETER_READS_AMOUNT 512
 
 #define FREQUENCY_IN_PIN A7
 
@@ -13,7 +13,6 @@
 #define FREQUENCY_OUT_DIO_PIN 3
 
 TEA5767 radio;
-GyverTM1637 frequencyDisplay(FREQUENCY_OUT_CLK_PIN, FREQUENCY_OUT_DIO_PIN);
 
 int minFrequency;
 int maxFrequency;
@@ -26,27 +25,21 @@ void setup()
   delay(200);
 
   radio.init();
-  //radio.debugEnable();
   radio.setBand(RADIO_BAND_FM);
-  radio.setVolume(2);
-  //radio.setMono(false);
 
   minFrequency = radio.getMinFrequency();
   maxFrequency = radio.getMaxFrequency();
-  frequencyStep = radio.getFrequencyStep();
-
-  frequencyDisplay.clear();
-  frequencyDisplay.brightness(7);
+  frequencyStep = 2; //radio.getFrequencyStep();
 }
 
 void loop()
 {
   long frequencyPotentiometerValue = 0;
-  for (int i = 0; i < 64; i++)
+  for (int i = 0; i < POTENTIOMETER_READS_AMOUNT; i++)
   {
     frequencyPotentiometerValue += analogRead(FREQUENCY_IN_PIN);
   }
-  frequencyPotentiometerValue = frequencyPotentiometerValue / 64;
+  frequencyPotentiometerValue = frequencyPotentiometerValue / POTENTIOMETER_READS_AMOUNT;
 
   int newFrequency = map(frequencyPotentiometerValue, MIN_POTENTIOMETER_VALUE, MAX_POTENTIOMETER_VALUE, minFrequency, maxFrequency);
 
@@ -54,23 +47,6 @@ void loop()
   {
     radio.setFrequency(newFrequency);
     currentFrequency = newFrequency;
-    frequencyDisplay.displayInt(currentFrequency);
     Serial.println(newFrequency);
   }
-
-  /*
-   char s[12];
-   radio.formatFrequency(s, sizeof(s));
-   Serial.print("Station:");
-   Serial.println(s);
-
-   Serial.print("Radio:");
-   radio.debugRadioInfo();
-
-   Serial.print("Audio:");
-   radio.debugAudioInfo();
-
-*/
-
-  //delay(3000);
 }
